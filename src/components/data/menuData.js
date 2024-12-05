59c306868bc7e830c4ac2cec4bf2a9176b8f5c9d
Loxt_Nixo <128501463+loxt-nixo@users.noreply.github.com>
2024-12-05 17:59:21 +0100

    Update menuData.js

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,5 +1,5 @@
 export const mainMenu = [
      {link:"#", content:"Home", icon:"lucide:home"},
      {link:"#about", content:"About", icon:"octicon:person-24"},
-     {link:"#skills", content:"Skills", icon: "" }
+     {link:"#skills", content:"Skills", icon: "octicon:beaker-24" }
 ]
